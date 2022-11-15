21301e185e461730fcae0b79bd6d7c31ba494ec3
RichMix | Chopp_Sueyy <82078286+RichMix@users.noreply.github.com>
2022-11-14 21:40:46 -0800

    Add files via upload

---
+++ LandingSection.js
@@ -0,0 +1,39 @@
+import React from "react"; 
+import { Avatar, Heading, VStack } from "@chakra-ui/react"; 
+import FullScreenSection from "./FullScreenSection"; 
+ 
+const greeting = "Hello, I am @Chopp_Sueyy!"; 
+const bio1 = "A horrible junior developer"; 
+const bio2 = "Trying really hard to specialize in React"; 
+ 
+const LandingSection = () => ( 
+ <FullScreenSection 
+   justifyContent="center" 
+   alignItems="center" 
+   isDarkBackground 
+   backgroundColor="#2A4365" 
+ > 
+   <VStack spacing={16}> 
+     <VStack spacing={4} alignItems="center"> 
+       <Avatar 
+         src="https://i.pravatar.cc/150?img=7" 
+         size="2xl" 
+         name="Choppsiggity" 
+       /> 
+       <Heading as="h4" size="md" noOfLines={1}> 
+         {greeting} 
+       </Heading> 
+     </VStack> 
+     <VStack spacing={6}> 
+       <Heading as="h1" size="3xl" noOfLines={1}> 
+         {bio1} 
+       </Heading> 
+       <Heading as="h1" size="3xl" noOfLines={1}> 
+         {bio2} 
+       </Heading> 
+     </VStack> 
+   </VStack> 
+ </FullScreenSection> 
+); 
+ 
+export default LandingSection;
